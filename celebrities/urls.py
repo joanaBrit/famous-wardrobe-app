7e8b69ecf2407241a90c51ba75581f6210475dd1
joana <joanabrito216@gmail.com>
2023-10-08 21:29:37 +0100

    Added reviw urls and did generics on celebrities views

--- celebrities/urls.py
+++ celebrities/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 # Path - /api/celebrities/
 urlpatterns = [
     path('', CelebrityListView.as_view()),
-    path('<int:id>/', CelebrityDetailView.as_view())
+    path('<int:pk>/', CelebrityDetailView.as_view())
 ]
--- celebrities/views.py
+++ celebrities/views.py
@@ -1,13 +1,12 @@
 
-# from rest_framework.generics import (
-#     RetrieveUpdateDestroyAPIView, 
-#     UpdateAPIView, 
-#     GenericAPIView
-# )
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.exceptions import NotFound
+from rest_framework.generics import (
+    RetrieveUpdateDestroyAPIView, 
+    ListCreateAPIView
+)
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+# from rest_framework.exceptions import NotFound
 
 # Model
 from .models import Celebrity
@@ -17,64 +16,76 @@ from .serializers.common import CelebritySerializer
 from .serializers.populated import PopulatedCelebritySerializer
 
 
-# EndPoint for view: /celebrities
-class CelebrityListView(APIView):
-    def get(self, request):
-      celebrities = Celebrity.objects.all()
-      serialized_celebrities = CelebritySerializer(celebrities, many=True)
-      return Response(serialized_celebrities.data)
-# class CelebrityListView(GenericAPIView):
-#     # 1 - Query the database to get all celebrities
-#     celebrities = Celebrity.objects.all()
-#     # 2 - We need to convert the data into python data type
-#     serialized_celebrities = CelebritySerializer(celebrities, many=True)
+# Class Views
+# Get celebrities
+class CelebrityListView(ListCreateAPIView):
+    queryset = Celebrity.objects.all()
+    serializer_class = CelebritySerializer
+    
+# Update, Delete Celebritys
+class CelebrityDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Celebrity.objects.all()
+    serializer_class = PopulatedCelebritySerializer
+
+
+
+# class CelebrityListView(APIView):
+#     def get(self, request):
+#       celebrities = Celebrity.objects.all()
+#       serialized_celebrities = CelebritySerializer(celebrities, many=True)
+#       return Response(serialized_celebrities.data)
+# # class CelebrityListView(GenericAPIView):
+# #     # 1 - Query the database to get all celebrities
+# #     celebrities = Celebrity.objects.all()
+# #     # 2 - We need to convert the data into python data type
+# #     serialized_celebrities = CelebritySerializer(celebrities, many=True)
     
   
-    def post(Self, request):
-      try:
+#     def post(Self, request):
+#       try:
   
-          celebrity_to_add = CelebritySerializer(data=request.data)
-          if celebrity_to_add.is_valid():
-              print(celebrity_to_add.validated_data)
-              celebrity_to_add.save()
-              return Response (celebrity_to_add.data, status.HTTP_201_CREATED)
-          print(celebrity_to_add.errors)
-          return Response(celebrity_to_add.errors, status.HTTP_422_UNPROCESSABLE_ENTITY)
-      except Exception as e :
-          print('exception ->', e)
-          return Response({ 'detail': str(e) }, status.HTTP_500_INTERNAL_SERVER_ERROR)
+#           celebrity_to_add = CelebritySerializer(data=request.data)
+#           if celebrity_to_add.is_valid():
+#               print(celebrity_to_add.validated_data)
+#               celebrity_to_add.save()
+#               return Response (celebrity_to_add.data, status.HTTP_201_CREATED)
+#           print(celebrity_to_add.errors)
+#           return Response(celebrity_to_add.errors, status.HTTP_422_UNPROCESSABLE_ENTITY)
+#       except Exception as e :
+#           print('exception ->', e)
+#           return Response({ 'detail': str(e) }, status.HTTP_500_INTERNAL_SERVER_ERROR)
       
-# EndPoint for view: /celebritie/pk      
-class CelebrityDetailView(APIView):
-    def get_celebrity(self, id):
-      try:
-          return Celebrity.objects.get(pk=id)
-        # print(designer)
-      except Celebrity.DoesNotExist as e:
-          raise NotFound(str(e))
+# # EndPoint for view: /celebritie/pk      
+# class CelebrityDetailView(APIView):
+#     def get_celebrity(self, id):
+#       try:
+#           return Celebrity.objects.get(pk=id)
+#         # print(designer)
+#       except Celebrity.DoesNotExist as e:
+#           raise NotFound(str(e))
       
-    # The url param
-    def get(self, request, id):
-      try:
-          celebrity = self.get_celebrity(id)
-          serialized_celebrity = PopulatedCelebritySerializer(celebrity)
-          return Response(serialized_celebrity.data) # you don't need to do like in the documentation data={} because this is the 2 param
-      except Exception as e:
-          print('Exception', e)
-          return Response({ 'error': str(e) }, status.HTTP_500_INTERNAL_SERVER_ERROR)
+#     # The url param
+#     def get(self, request, id):
+#       try:
+#           celebrity = self.get_celebrity(id)
+#           serialized_celebrity = PopulatedCelebritySerializer(celebrity)
+#           return Response(serialized_celebrity.data) # you don't need to do like in the documentation data={} because this is the 2 param
+#       except Exception as e:
+#           print('Exception', e)
+#           return Response({ 'error': str(e) }, status.HTTP_500_INTERNAL_SERVER_ERROR)
       
-    # Update
-    def put(self, request, id):
-          celebrity = self.get_celebrity(id)
-          serialized_celebrity = CelebritySerializer(celebrity, request.data, partial=True)
-          if serialized_celebrity.is_valid():
-              serialized_celebrity.save()
-              return Response (serialized_celebrity.data, status.HTTP_200_OK)
-          return Response(serialized_celebrity.errors, status.HTTP_422_UNPROCESSABLE_ENTITY)
+#     # Update
+#     def put(self, request, id):
+#           celebrity = self.get_celebrity(id)
+#           serialized_celebrity = CelebritySerializer(celebrity, request.data, partial=True)
+#           if serialized_celebrity.is_valid():
+#               serialized_celebrity.save()
+#               return Response (serialized_celebrity.data, status.HTTP_200_OK)
+#           return Response(serialized_celebrity.errors, status.HTTP_422_UNPROCESSABLE_ENTITY)
     
-    # Delete
-    def delete(self, request, id):
-        celebrity_to_delete = self.get_celebrity(id)
-        celebrity_to_delete.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     # Delete
+#     def delete(self, request, id):
+#         celebrity_to_delete = self.get_celebrity(id)
+#         celebrity_to_delete.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
       
